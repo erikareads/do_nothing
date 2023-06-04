f3bf63ff2d84143c42b9af7599137f2c03c6e8c4
Erika Rowland <accounts@erikarowland.com>
2023-06-03 18:36:03 -0700

    chore: Update example

--- examples/tidy.exs
+++ examples/tidy.exs
@@ -1,25 +1,18 @@
 #!/usr/bin/env elixir
 
-Mix.install([{:do_nothing, git: "git@github.com:erikareads/do_nothing.git"}])
+Mix.install([{:do_nothing, github: "erikareads/do_nothing", tag: "v0.1.0"}])
 
-defmodule Tidy do
-  use DoNothing
-
-  title "Tidy apartment"
-  description "Keep track of the steps required to tidy an apartment"
-
-  step do
-    id :pickup_trash
-    title "Pick up all trash on floor"
-    instructions "Grab a trash bag and pick up all non-paper trash on the floor of the apartment"
-  end
-
-  step do
-    id :pickup_recycling
-    title "Pick up all recycling on the floor"
-
-    instructions "Pick the largest box. Break down all boxes that are smaller than that and insert. Take the larger box to the recycling."
-  end
-end
-
-DoNothing.execute(Tidy)
+DoNothing.procedure(
+  title: "Tidy apartment",
+  description: "Keep track of the steps required to tidy an apartment"
+)
+|> DoNothing.add_step(
+  title: "Pick up all trash on the floor",
+  instructions: "Grab a trash bag and pick up all non-paper trash on the floor of the apartment."
+)
+|> DoNothing.add_step(
+  title: "Pick up all recycling on the floor",
+  instructions:
+    "Pick the largest box. Break down all boxes that are smaller than that and insert. Take the larger box to the recycling."
+)
+|> DoNothing.execute()
